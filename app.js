745f1584a2176fd284227712ac578f313cb59471
Abraham <abraham.attia.pro@gmail.com>
2024-04-15 18:01:52 +0200

    - creation de notre route get dans app.js qui contien notre tableau de  produits -  gestion de l'erreur CORS (Cross Origin Resource Sharing) par l'ajout des headers

--- app.js
+++ app.js
@@ -7,28 +7,44 @@ const app = express();
 
 //middleware = fonction express qui reçoit la requête req et la réponse res, qui les geres, et qui eventuellement passe la main au middleware suivant next, pour continuer l'execution du serveur en utilisant la méthode next() en fin de fonction  
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-  });
-  
-  app.use((req, res, next) => {
-    res.status(201);
-    console.log('statut 201 envoyé !');
-    next();
-  });
-
+//middleware general qui ne concerne pas de route spécifique
+//middleware pour eviter les erreurs CORS (Cross Origin Resource Sharing)
 app.use((req, res, next) => 
 {
-    //retourne une reponse avec un message au format json
-    res.json({ message: 'Votre requête a bien été reçue !' });
+    //autoriser l'accès à notre API depuis n'importe quelle origine ( '*' )
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    //autoriser l'utilisation de certains en tete (Origin , X-Requested-With , etc.)
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    //autoriser l'utilisation de certaines méthodes (GET, POST, PUT, DELETE, PATCH, OPTIONS)
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
-
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
   });
+
+//app.use attribut un middleware à une route spécifique
+app.use('/api/stuff', (req, res, next) => 
+{
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    //attribuer un statut de réponse 200 (succès) à la réponse et envoyer le tableau contenant les objets au format JSON en tant que réponse
+    res.status(200).json(stuff);
+  });
+  
   
 //exporter l'application express pour y accéder depuis les autres fichiers (ex notre serveur node)
 module.exports = app;
