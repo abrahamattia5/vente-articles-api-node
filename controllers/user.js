34285b78b4278ee0b64ac82042a660d77a1b03c6
Abraham <abraham.attia.pro@gmail.com>
2024-05-19 19:23:12 +0200

    implementation de la fonction login du controllers/user.js pour qu'un utilisateur s'etant enregistrer ( avec la methode signup) puisse s'authentifier et ce connecter
    - verfication des element des connexion email et password -> securisation du message d'erreur pour ne pas dire pourquoi la requette echou si le login ou le password est incorect
    
    - utilisation de bcrypt.compare pour comparer le mot de passe avec le hash
    
    -  en cas de connexion reussi mise en place du userId & du token

--- controllers/user.js
+++ controllers/user.js
@@ -30,5 +30,34 @@ exports.signup = (req, res, next) =>
 
 exports.login = (req, res, next) => 
 {
-    //
+    //on cherche dans la BDD si le champ email d'un utilisateur correspond au email envoyé dans la requête
+    User.findOne({ email: req.body.email })
+        //si la fonction findOne n'echoue pas, on recupere la valeur retournée par notre requête dans une promesse (user)
+        .then(user => 
+        {
+            //si aucun utilisateur n'est trouvé avec cet email
+            if (!user) 
+            {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            //si non (un utilisateur est trouvé avec cet email), on compare le mot de passe envoyé dans la requête avec le hash enregistré dans la BDD pour cet utilisateur (fonction compare de bcrypt
+            bcrypt.compare(req.body.password, user.password)
+                //si la fonction compare ne renvoie pas d'erreur, elle renvoie un booléen qui indique si le mot de passe est le bon ou non
+                .then(valid => 
+                {
+                    if (!valid)
+                    {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json(
+                    {
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                })
+                //si la fonction compare renvoie une erreur
+                .catch(error => res.status(500).json({ error }));
+        })
+        //si la fonction findOne ne reussi pas (erreur d'execution dans la BDD)
+        .catch(error => res.status(500).json({ error }));
 };
